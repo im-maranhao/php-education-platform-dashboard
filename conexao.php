bfbc64db373cec21c1a631fed719152f764c42c1
Miriam Maranhão Da Silva <98363007+im-maranhao@users.noreply.github.com>
2026-01-06 23:57:57 -0300

    Atualizar o conexao.php

--- conexao.php
+++ conexao.php
@@ -5,7 +5,9 @@ define('DB_SERVER', '127.1.1' );
 define('DB_USERNAME', 'u244070043_irtvm' );
 
 define('DB_PASSWORD', 'T123456m' );
+
 define('DB_NAME', 'u244070043_irtvm' );
+
 $conexao = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
 if ($conexao->connect_error) {
