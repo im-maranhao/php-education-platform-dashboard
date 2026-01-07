a9b53efb13bb046b96a957b1552ba2c442f5f34b
Miriam Maranhão Da Silva <98363007+im-maranhao@users.noreply.github.com>
2026-01-06 23:58:18 -0300

    Atualizar o conexao.php

--- conexao.php
+++ conexao.php
@@ -8,7 +8,9 @@ define('DB_PASSWORD', 'T123456m' );
 
 define('DB_NAME', 'u244070043_irtvm' );
 
-$conexao = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+$conexao = mysqli_connect(DB_SERVER,
+
+DB_USERNAME, DB_PASSWORD, DB_NAME);
 
 if ($conexao->connect_error) {
     die("Connection failed: " . $conexao->connect_error);
